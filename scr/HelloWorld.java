8813599f5633cb53596b572ed8e89dcf62c26c52
wchan372 <59932696+wchan372@users.noreply.github.com>
2020-01-15 11:50:05 -0700

    Create HelloWorld.java

---
+++ scr/HelloWorld.java
@@ -0,0 +1,9 @@
+public class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, world.");
+		System.out.println("Commit by Wei Chen!!");
+	}
+
+}
